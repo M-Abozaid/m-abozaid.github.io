a7010d90bc7b2791a0f6502e2e87ccc0e7578091
markmabery <markdennismabery@gmail.com>
2024-08-03 18:01:39 +0300

    fix: typo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Muhammed</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I a Full Stack Developer, <br className="sm:block hidden" />
+            I'm a Full Stack Developer, <br className="sm:block hidden" />
             hacker, and tech enthusiast.
           </p>
         </div>
